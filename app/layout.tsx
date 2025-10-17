2986c49c8c6540609cced2687ce236b304a42811
Olcay Elikci <olcay.elikci@hotmail.com>
2025-10-17 15:33:58 +0200

    Google Tag Manager hinzugefügt
    - GTM-KK359VS8 Container ID implementiert
    - Script im <head> Bereich hinzugefügt
    - Noscript Fallback im <body> implementiert
    - Zusätzlich zum bestehenden Google Analytics
    - Ermöglicht erweiterte Tracking-Funktionen

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,16 @@ export default function RootLayout({
   return (
     <html lang="de" dir="ltr">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KK359VS8');`}
+        </Script>
+        {/* End Google Tag Manager */}
+        
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#4f46e5" />
@@ -99,6 +109,17 @@ export default function RootLayout({
       <body
         className={`${inter.variable} font-sans antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KK359VS8"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
         <CookieBanner />
 
